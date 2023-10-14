82c1aae9dfce63eb41b774e22cb595e4c42567f0
bvilane <b.vilane@alustudent.com>
2023-10-14 23:32:49 +0200

    Latest Bavu Commit for Menu Nav Bar

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const nav = document.querySelector('nav');
 
     if (menuIcon && nav) {
-        menuIcon.addEventListener('click', function () {
-            if (window.innerWidth <= 600) { // Adjust this value based on your media query
-                nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'flex' : 'none';
+        menuIcon.addEventListener('click', function (event) {
+            nav.classList.toggle('active');
+            event.stopPropagation();
+        });
+
+        // Add event listener to close the nav when the window is clicked outside of it
+        document.addEventListener('click', function (event) {
+            if (!nav.contains(event.target) && window.innerWidth <= 600) {
+                nav.classList.remove('active');
+            }
+        });
+
+        // Add event listener to close the nav when the window is resized to a larger screen
+        window.addEventListener('resize', function () {
+            if (window.innerWidth > 600) {
+                nav.classList.remove('active');
             }
         });
     }
